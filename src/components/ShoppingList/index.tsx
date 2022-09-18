d3de742991a204ec6c05a08fa0938d5a15e2117f
Anna Maria <aannamariabr@outlook.com>
2022-09-18 02:25:49 -0300

    Utilizando limit para limitar retorno da consulta de produtos

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -13,6 +13,7 @@ export function ShoppingList() {
   useEffect(() => {
     const subscribe = firestore()
     .collection('products')
+    //.limit(3)
     //.where('quantity', '==', 1)
     .onSnapshot(querySnapshot => {
       const data = querySnapshot.docs.map((doc) => {
